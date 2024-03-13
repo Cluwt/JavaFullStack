e75a1c529faa322d0331cb5eacac0393d4cbb8a3
Cluwt <csar.synderal@gmail.com>
2024-03-13 19:33:59 -0300

    Segundo commit

---
+++ Lógica de Programação/Variáveis.java
@@ -0,0 +1,32 @@
+public class Variáveis {
+    public static void main(String[] args) {
+
+        // String Comum:
+        String Sobrenome = "Ribeiro";
+
+        // Tamanho de 8 bits, -128 ~ 127 e padrão de 0:
+        byte b = 127; 
+
+        // Tamanho de 16 bits, -32768 ~ 32767 e padrão 0:
+        short s = 666;
+
+        // Tamanho de 32 bits, -214783648 ~ 214783678 e padrão 0:
+        int i = 333; 
+
+        // Tamanho de 64 bits, valor e tamanho imensos, com padrão 0;
+        long l = 66666666;
+
+        // Tamanho de 32 bits, com valores com vírgula, com padrão de 0.0f
+        float f = 666.666f;
+
+        // Tamanho de 64 bits, valores altíssimos com padrão de 0.0
+        double d = 666.666;
+
+        // Tamanho de 16 bits, valores em char
+        char c = 'a';
+
+        // Booleano ou lógico
+        boolean bool = true;
+    }
+    
+}
---
+++ Lógica-de-Programação/OperadoresAritméticos.java
@@ -0,0 +1,6 @@
+public class OperadoresAritméticos{
+    public static void main(String[] args) {
+        int resultado = 1 + 2 - 3 * 4 / 5;
+        System.out.println(resultado);
+    }
+}
