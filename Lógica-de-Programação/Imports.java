c23682c6f20b39f99d6a77ad0b31874c5cfd432a
Cluwt <csar.synderal@gmail.com>
2024-03-13 19:41:10 -0300

    Terceiro commit

---
+++ Lógica-de-Programação/Imports.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.*;
+import org.xml.sax.*;
+
+public class Imports {
+    public static void main(String[] args) {
+        System.out.println(new Random().nextInt());
+        System.out.println(Math.random() * 10);
+
+        Scanner ler = new Scanner(System.in);
+    }
+}
